9de1faf4568518c4044461ab97b6a62837cf78f0
kim kyugnwon <gwon080870@gmail.com>
2023-06-12 23:33:29 +0900

    FEAT: Add const

--- lib/features/presentation/pages/home_page/home_page.dart
+++ lib/features/presentation/pages/home_page/home_page.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('기억 창고'),
+        title: const Text('기억 창고'),
       ),
     );
   }
