467162d326d1003b56cc629accb8e3b1f170f141
kim kyugnwon <gwon080870@gmail.com>
2023-06-12 23:36:44 +0900

    FEAT: Add Initial UI

--- lib/features/presentation/pages/home_page/home_page.dart
+++ lib/features/presentation/pages/home_page/home_page.dart
@@ -9,6 +9,16 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('기억 창고'),
       ),
+      body: CalendarDatePicker(
+        initialDate: DateTime.now(),
+        firstDate: DateTime(1900),
+        lastDate: DateTime(2300),
+        onDateChanged: (value) {},
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
