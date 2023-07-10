e375b55f6c51b7a405fb25b0e730df24679d7b8e
kim kyugnwon <gwon080870@gmail.com>
2023-07-10 23:31:37 +0900

    FEAT: Add Gallery

---
+++ lib/features/presentation/pages/add_gallery_page/add_gallery_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddGalleryPage extends StatelessWidget {
+  const AddGalleryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
