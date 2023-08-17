e44a0a02f9a62eb1f4bb96583ede7bb83466053a
kim kyugnwon <gwon080870@gmail.com>
2023-08-17 23:59:49 +0900

    FEAT: Change date

--- lib/features/presentation/pages/add_page/local_widgets/date_text_button.dart
+++ lib/features/presentation/pages/add_page/local_widgets/date_text_button.dart
@@ -1,20 +1,45 @@
 import 'package:flutter/material.dart';
 
-class DateTextButton extends StatelessWidget {
+class DateTextButton extends StatefulWidget {
   const DateTextButton({super.key});
+
+  @override
+  State<DateTextButton> createState() => _DateTextButtonState();
+}
+
+class _DateTextButtonState extends State<DateTextButton> {
+  late DateTime _date;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _date = DateTime.now();
+  }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      child: Text(DateTime.now().toString().split(' ')[0]),
-      onTap: () {
-        showDatePicker(
+      child: Text(dateTimeToString()),
+      onTap: () => _onTap(),
+    );
+  }
+
+  void _onTap() async {
+    DateTime? date = await showDatePicker(
           context: context,
-          initialDate: DateTime.now(),
+          initialDate: _date,
           firstDate: DateTime(1900),
           lastDate: DateTime(2300),
-        );
-      },
-    );
+        ) ??
+        _date;
+
+    setState(() {
+      _date = date;
+    });
+  }
+
+  String dateTimeToString() {
+    return _date.toString().split(' ')[0];
   }
 }
