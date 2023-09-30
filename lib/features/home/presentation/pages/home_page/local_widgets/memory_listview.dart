7899733df99b5956004dcdbe8d846b6a2da2b09a
kim kyugnwon <gwon080870@gmail.com>
2023-09-30 16:52:12 +0900

    FEAT: Insert memory on container entity

--- lib/features/home/presentation/pages/home_page/local_widgets/memory_listview.dart
+++ lib/features/home/presentation/pages/home_page/local_widgets/memory_listview.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:memory_conatiner/core/shared_feature/domain/entity/memory_entity.dart';
+import 'package:memory_conatiner/features/home/domain/entity/container_entity.dart';
 import 'package:memory_conatiner/features/home/presentation/pages/home_page/local_widgets/memory_item.dart';
 import 'package:memory_conatiner/features/home/presentation/presenster/home_view_model.dart';
 
@@ -9,7 +10,8 @@ class MemoryListView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    List<MemoryEntity> memories = ref.watch(homeViewModelProvider);
+    List<MemoryEntity> memories =
+        ref.watch<ContainerEntity>(homeViewModelProvider).memories;
 
     return Expanded(
       child: ListView.builder(
