f19171cf99f0e5b86c8367d1475dea6b31bdd670
kim kyugnwon <gwon080870@gmail.com>
2023-09-30 15:12:20 +0900

    FEAT: Copy with key

--- lib/core/shared_feature/domain/entity/memory_entity.dart
+++ lib/core/shared_feature/domain/entity/memory_entity.dart
@@ -14,11 +14,13 @@ class MemoryEntity extends Equatable {
   });
 
   MemoryEntity copyWith({
+    int? key,
     String? title,
     String? content,
     String? date,
   }) {
     return MemoryEntity(
+      key: key ?? this.key,
       title: title ?? this.title,
       content: content ?? this.content,
       date: date ?? this.date,
