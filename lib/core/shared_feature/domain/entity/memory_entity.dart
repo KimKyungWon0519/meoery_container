7c02da9cb9ac9defec2d5d0d579921000d190d19
kim kyugnwon <gwon080870@gmail.com>
2023-11-07 00:26:12 +0900

    FEAT: Add Galleries member variable

--- lib/core/shared_feature/domain/entity/memory_entity.dart
+++ lib/core/shared_feature/domain/entity/memory_entity.dart
@@ -1,16 +1,19 @@
 import 'package:equatable/equatable.dart';
+import 'package:memory_conatiner/features/add/domain/entity/gallery_entity.dart';
 
 class MemoryEntity extends Equatable {
   final int key;
   final String title;
   final String content;
   final String date;
+  final List<GalleryEntity> galleries;
 
   const MemoryEntity({
     this.key = -1,
     this.title = '',
     this.content = '',
     this.date = '',
+    this.galleries = const [],
   });
 
   MemoryEntity copyWith({
@@ -18,12 +21,14 @@ class MemoryEntity extends Equatable {
     String? title,
     String? content,
     String? date,
+    List<GalleryEntity>? galleries,
   }) {
     return MemoryEntity(
       key: key ?? this.key,
       title: title ?? this.title,
       content: content ?? this.content,
       date: date ?? this.date,
+      galleries: galleries ?? this.galleries,
     );
   }
 
